52f81edf172489b01ded72e21e00fed447746176
Paul-Inge Flakstad <paulflakstad@gmail.com>
2016-06-16 16:41:19 +0200

    Fix ampersand escaping and parameter handling. Add comments.

--- src/no/npolar/data/api/SearchResultsPagination.java
+++ src/no/npolar/data/api/SearchResultsPagination.java
@@ -1,10 +1,13 @@
 package no.npolar.data.api;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import no.npolar.data.api.util.APIUtil;
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Search results navigation: pagination – previous / page numbers / next.
@@ -12,38 +15,77 @@ import org.apache.commons.lang.StringEscapeUtils;
  * @author Paul-Inge Flakstad, Norwegian Polar Institute <flakstad at npolar.no>
  */
 public class SearchResultsPagination {
+    /** Total results for the search. */
     private int totalResults = -1;
+    /** Items per page for the search. */
     private int itemsPerPage = -1;
+    /** Start index for the search. */
     private int startIndex = -1;
+    /** Current page number for the search. */
     private int pageNumber = -1;
+    /** Total number of pages for the search. */
     private int pagesTotal = -1;
     
+    /** 
+     * Base URI for the web page that displays the search results. 
+     * <p>
+     * It is named "base URI" because it should be empty of any service-related 
+     * parameters, and ready to have search parameters appended.
+     * <p>
+     * So, for example:
+     * <ul>
+     * <li>/my/serp.html</li>
+     * <li>/my/serp.html?foo=bar</li>
+     * <li>NOT <del>/my/serp.html?q=foo&start=10</del></li>
+     * </ul>
+     */
     private String baseUri = null;
+    /** The character used when appending parameters, either &amp; or ?. */
     private String parameterAppender = null;
+    /** The set of parameters, as a parameter string, that can be used to get the next page. */
     private String next = null;
+    /** The set of parameters, as a parameter string, that can be used to get the previous page. */
     private String prev = null;
     
+    /** The service's freely modifiable parameters. */
     private Map<String, String[]> serviceParamsFree = null;
+    /** The service's locked (non-modifiable) parameters. */
     private Map<String, String[]> serviceParamsLocked = null;
     
+    /** The logger. */
+    private static final Log LOG = LogFactory.getLog(no.npolar.data.api.SearchResultsPagination.class);
+    
+    /** Class name for the entire pagination component. */
     public static final String DEFAULT_CLASS_PAGINATION = "pagination";
+    /** Class name for the "info" element inside pagination. */
     public static final String DEFAULT_CLASS_PAGINATION_INFO = DEFAULT_CLASS_PAGINATION + "__info";
+    /** Class name for the "truncated pages" element inside pagination. */
     public static final String DEFAULT_CLASS_TRUNCATION = DEFAULT_CLASS_PAGINATION + "__truncation";
+    /** Class name for page elements inside pagination. */
     public static final String DEFAULT_CLASS_PAGE = DEFAULT_CLASS_PAGINATION + "__page";
+    /** Class name for page wrapper elements inside pagination. */
     public static final String DEFAULT_CLASS_PAGE_WRAPPER = DEFAULT_CLASS_PAGE + "-wrap";
+    /** Class name for the previous page element inside pagination. */
     public static final String DEFAULT_CLASS_PAGE_PREV = APIUtil.classMod(DEFAULT_CLASS_PAGE, "prev") + " prev";
     //public static final String DEFAULT_CLASS_PAGE_PREV = DEFAULT_CLASS_PAGE + " " + DEFAULT_CLASS_PAGE + "--prev" + " prev";
+    /** Class name for the next page element inside pagination. */
     public static final String DEFAULT_CLASS_PAGE_NEXT = APIUtil.classMod(DEFAULT_CLASS_PAGE, "next") + " next";
     //public static final String DEFAULT_CLASS_PAGE_NEXT = DEFAULT_CLASS_PAGE + " " + DEFAULT_CLASS_PAGE + "--next" + " next";
+    /** Class name for numbered page elements inside pagination. */
     public static final String DEFAULT_CLASS_PAGE_NUMBERED = APIUtil.classMod(DEFAULT_CLASS_PAGE, "numbered");
     //public static final String DEFAULT_CLASS_PAGE_NUMBERED = DEFAULT_CLASS_PAGE + " " + DEFAULT_CLASS_PAGE + "--numbered";
+    /** Class name for the current page element inside pagination. */
     public static final String DEFAULT_CLASS_PAGE_CURRENT = APIUtil.classMod(DEFAULT_CLASS_PAGE, "numbered", "current") + " currentpage";
     //public static final String DEFAULT_CLASS_PAGE_CURRENT = DEFAULT_CLASS_PAGE_NUMBERED + " " + DEFAULT_CLASS_PAGE + "--current" + " currentpage";
+    /** Class name for inactive elements inside pagination. */
     public static final String DEFAULT_CLASS_INACTIVE = "inactive";
+    /** Class name for the previous page wrapper element inside pagination. */
     public static final String DEFAULT_CLASS_PAGE_PREV_WRAPPER = APIUtil.classMod(DEFAULT_CLASS_PAGE_WRAPPER, "prev") + " pagePrevWrap";
     //public static final String DEFAULT_CLASS_PAGE_PREV_WRAPPER = DEFAULT_CLASS_PAGE_WRAPPER + " " + DEFAULT_CLASS_PAGE_WRAPPER + "--prev" + " pagePrevWrap";
+    /** Class name for the next page wrapper element inside pagination. */
     public static final String DEFAULT_CLASS_PAGE_NEXT_WRAPPER = APIUtil.classMod(DEFAULT_CLASS_PAGE_WRAPPER, "next") + " pageNextWrap";
     //public static final String DEFAULT_CLASS_PAGE_NEXT_WRAPPER = DEFAULT_CLASS_PAGE_WRAPPER + " " + DEFAULT_CLASS_PAGE_WRAPPER + "--next" + " pageNextWrap";
+    /** Class name for the numbered pages wrapper element inside pagination. */
     public static final String DEFAULT_CLASS_PAGES_NUMBERED_WRAPPER = APIUtil.classMod(DEFAULT_CLASS_PAGE_WRAPPER, "numbers") + " pageNumWrap";
     //public static final String DEFAULT_CLASS_PAGES_NUMBERED_WRAPPER = DEFAULT_CLASS_PAGE_WRAPPER + " " + DEFAULT_CLASS_PAGE_WRAPPER + "--numbers"+ " pageNumWrap";
     
@@ -52,7 +94,8 @@ public class SearchResultsPagination {
      * {@link APIService} instance, and using the base URI as a foundation for 
      * all pagination links.
      * <p>
-     * The base URI is typically the "current" URI.
+     * The base URI is typically the "current" URI, without any parameters. 
+     * However, it should be safe to pass parameters as well, if necessary.
      * 
      * @param service The {@link APIService} instance to base the pagination on.
      * @param baseUri The URI to use as foundation for all pagination links, typically the "current" URI.
@@ -61,18 +104,48 @@ public class SearchResultsPagination {
         totalResults = service.getTotalResults();
         itemsPerPage = service.getItemsPerPage();
         startIndex = service.getStartIndex();
+        
         this.baseUri = baseUri;
-        parameterAppender = baseUri.contains("?") ? "&amp;" : "?";
+        parameterAppender = baseUri.contains("?") ? "&" : "?";
+        
         try {
             serviceParamsFree = service.getParameters();
             serviceParamsLocked = service.getPresetParameters();
+            
+            try {
+                // Retain any "extra" parameters present in the base URI (that 
+                // is, any parameter not related to the service)
+                Map<String, String> baseUriParams = APIUtil.getParametersInQueryString(baseUri);
+                if (!baseUriParams.isEmpty()) {
+                    Iterator<String> iBaseUriParams = baseUriParams.keySet().iterator();
+                    while (iBaseUriParams.hasNext()) {
+                        String key = iBaseUriParams.next();
+                        if (serviceParamsFree.containsKey(key) || serviceParamsLocked.containsKey(key)) {
+                            iBaseUriParams.remove();
+                        }
+                    }
+                    this.baseUri = baseUri.split("\\?")[0];
+                    if (baseUriParams.isEmpty()) {
+                        parameterAppender = "?";
+                    } else {
+                        this.baseUri += "?" + APIUtil.getParameterString(baseUriParams);
+                        parameterAppender = "&";
+                    }
+                }
+            } catch (Exception ee) {
+                if (LOG.isErrorEnabled()) {
+                    LOG.error("Error processing base URI with parameters, intended for pagination.", ee);
+                }
+            }
             pageNumber = (startIndex + itemsPerPage) / itemsPerPage;
             pagesTotal = (int)(Math.ceil((double)(totalResults + itemsPerPage) / itemsPerPage)) - 1;
 
             next = service.getNextPageParameters();
             prev = service.getPrevPageParameters();
         } catch (Exception e) {
-            // Ignore
+            if (LOG.isErrorEnabled()) {
+                LOG.error("Error creating pagination.", e);
+            }
         }
     }
     
@@ -147,7 +220,7 @@ public class SearchResultsPagination {
      * which will return <code>null</code> if there is no next page.)
      * 
      * @param clazz The class to apply to the link.
-     * @param uri The link target (<code>href</code> attribute), properly escaped.
+     * @param uri The link target (<code>href</code> attribute), unescaped.
      * @param missingUriClass The class to apply to the link in case the URI is empty or <code>null</code>.
      * @return The ready-to-use link.
      */
@@ -157,53 +230,74 @@ public class SearchResultsPagination {
             link += " " + missingUriClass;
         }
         else {
-            link += "\" href=\"" + uri;
+            link += "\" href=\"" + StringEscapeUtils.escapeHtml(uri);
         }
         link += "\"></a>"; // ToDo: Add text here (not accessible as of now) => requires some localization...
         return link;
     }
     
+    /**
+     * Appends the given parameter string to the base URI.
+     * 
+     * @param parameterString The parameter string to append.
+     * @return The base URI, with the given parameter string appended.
+     */
     public String appendToBase(String parameterString) {
-        return baseUri + parameterAppender + StringEscapeUtils.escapeHtml(parameterString);
+        return baseUri + parameterAppender + parameterString;
     }
     
+    /**
+     * Gets the in-between pages - that is, the numbered page span in-between 
+     * the "next" and "previous" page.
+     * 
+     * @return The in-between pages.
+     */
     public List<String> inBetweenPages() {
         List<String> pages = new ArrayList<String>();
-        for (int pageCounter = 1; pageCounter <= pagesTotal; pageCounter++) {
-            boolean splitNav = pagesTotal >= 8;
-            // if (first page OR last page OR (pages total > 10 AND (this page number > (current page number - 4) AND this page number < (current page number + 4)))
-            // Pseudo: if this is the first page, the last page, or a page close to the current page (± 4)
-            if (!splitNav
-                        || (splitNav && (pageCounter == 1 || pageCounter == pagesTotal))
-                        //|| (pagesTotal > 10 && (pageCounter > (pageNumber-4) && pageCounter < (pageNumber+4)))) {
-                        || (splitNav && (pageCounter > (pageNumber-3) && pageCounter < (pageNumber+3)))) {
-                if (pageCounter != pageNumber) {
-                    // Not the current page: print a link
-                    pages.add("<a class=\"" + DEFAULT_CLASS_PAGE_NUMBERED + "\" href=\"" 
-                            + appendToBase(
-                                    APIUtil.toParameterString(serviceParamsFree, APIService.Param.START_AT)
-                                    + "&" + APIService.Param.START_AT + "=" + ((pageCounter-1) * itemsPerPage)
-                            ) + "\">" + pageCounter + "</a>");
+        try {
+            for (int pageCounter = 1; pageCounter <= pagesTotal; pageCounter++) {
+                boolean splitNav = pagesTotal >= 8;
+                // if (first page OR last page OR (pages total > 10 AND (this page number > (current page number - 4) AND this page number < (current page number + 4)))
+                // Pseudo: if this is the first page, the last page, or a page close to the current page (± 4)
+                if (!splitNav
+                            || (splitNav && (pageCounter == 1 || pageCounter == pagesTotal))
+                            //|| (pagesTotal > 10 && (pageCounter > (pageNumber-4) && pageCounter < (pageNumber+4)))) {
+                            || (splitNav && (pageCounter > (pageNumber-3) && pageCounter < (pageNumber+3)))) {
+                    if (pageCounter != pageNumber) {
+                        // Not the current page: print a link
+                        pages.add("<a class=\"" + DEFAULT_CLASS_PAGE_NUMBERED + "\" href=\"" 
+                                + StringEscapeUtils.escapeHtml(
+                                        appendToBase(
+                                            APIUtil.toParameterString(serviceParamsFree, APIService.Param.START_AT)
+                                            + "&" + APIService.Param.START_AT + "=" + ((pageCounter-1) * itemsPerPage)
+                                        )
+                                )
+                                + "\">" + pageCounter + "</a>");
+                    }
+                    else {
+                        // The current page: no link
+                        pages.add("<span class=\"" + DEFAULT_CLASS_PAGE_CURRENT + "\">" + pageCounter + "</span>");
+                    }
                 }
-                else {
-                    // The current page: no link
-                    pages.add("<span class=\"" + DEFAULT_CLASS_PAGE_CURRENT + "\">" + pageCounter + "</span>");
+                // Pseudo: 
+                else if (splitNav && (pageCounter == 2 || pageCounter+1 == pagesTotal)) { 
+                    pages.add("<span class=\"" + DEFAULT_CLASS_TRUNCATION + "\"> &hellip; </span>");
+                } else {
+                    //pages.add("<!-- page " + pageCounter + " dropped ... -->");
                 }
             }
-            // Pseudo: 
-            else if (splitNav && (pageCounter == 2 || pageCounter+1 == pagesTotal)) { 
-                pages.add("<span class=\"" + DEFAULT_CLASS_TRUNCATION + "\"> &hellip; </span>");
-            } else {
-                //pages.add("<!-- page " + pageCounter + " dropped ... -->");
+        } catch (Exception e) {
+            if (LOG.isErrorEnabled()) {
+                LOG.error("Error creating the in-between pages for pagination.", e);
             }
         }
         return pages;
     }
     
     /**
-     * Gets the "standard" pagination HTML.
+     * Gets the default pagination HTML.
      * 
-     * @return  the "standard" pagination HTML.
+     * @return The default pagination HTML.
      */
     public String getPaginationHtml() {
         String s = "";
